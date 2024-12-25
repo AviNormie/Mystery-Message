9f598d67db0ea0503e5bd1a97c85d97fa9ea01a3
user.name <avi26022007@gmail.com>
2024-12-25 12:24:09 +0530

    adding Next Auth

--- src/app/api/auth/[...nextauth]/options.ts
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -1,1 +1,44 @@
-// import {NextAuthOptions}  from 
+import {NextAuthOptions}  from 'next-auth'
+import Credentials, { CredentialsProvider } from 'next-auth/providers/credentials'
+import bcrypt from'bcryptjs'
+import dbConnect from '@/lib/dbConnect'
+import UserModel  from '@/model/User'
+
+export const authoptions: NextAuthOptions = {
+    providers:[
+        CredentialsProvider({
+            id:'credentials',
+            name:'Credentials',
+            credentials:{
+                email:{label:'Email',type:'text'},
+                password:{label:'Password',type:'password'}
+            },
+            async authorize(credentials: any): Promise<any>{
+                await dbConnect()
+                try {
+                    const user =  await UserModel.findOne({
+                        $or:[
+                            {email:credentials.identifier},
+                            {username:credentials.identifier}
+                        ]
+                    })
+                    if(!user){
+                        throw new Error('No user found with this email ')
+                    }
+                    if(!user.isVerified){
+                        throw new Error('Please verify yout account before login  ')
+                    }
+
+                    const isPasswordCorrect = await bcrypt.compare(credentials.password,user.password )
+                    if(isPasswordCorrect){
+                        return user
+                    } else{
+                        throw new Error('Invalid  password ')
+                    }
+
+                } catch (err:any) {
+                    throw new Error(err)
+                }
+        }})
+    ]
+}
