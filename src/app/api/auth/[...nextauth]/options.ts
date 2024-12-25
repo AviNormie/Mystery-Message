0f9db445e98e8c76be600ea7e0300f99a8a49231
user.name <avi26022007@gmail.com>
2024-12-25 12:26:03 +0530

    CredentialProvider error solved

--- src/app/api/auth/[...nextauth]/options.ts
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -1,5 +1,6 @@
 import {NextAuthOptions}  from 'next-auth'
-import Credentials, { CredentialsProvider } from 'next-auth/providers/credentials'
+import CredentialsProvider from 'next-auth/providers/credentials'
+
 import bcrypt from'bcryptjs'
 import dbConnect from '@/lib/dbConnect'
 import UserModel  from '@/model/User'
